ac9fdc7d6383f15c705224e2063f87f0f961857d
garzodemierd <lautaro.garcia.et21.21@gmail.com>
2025-10-20 19:04:59 -0300

    package.json

---
+++ backend/src/models/books.js
@@ -0,0 +1,13 @@
+
+
+//obtener estructura libro x
+function getBooks() {
+    connection.execute
+}
+
+//obtener estructura todos los libros
+
+//obtener x dato de x libro
+
+//obtener x dato de todos los libros
+
