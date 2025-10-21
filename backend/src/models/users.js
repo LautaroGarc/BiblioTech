576f0ce2652a586af2bea3fae564a0a7c850016c
lautaroandradaet2121-spec <lautaro.andrada.et21.21@gmail.com>
2025-10-21 13:23:56 -0300

    added some users functions

--- backend/src/models/users.js
+++ backend/src/models/users.js
@@ -1,1 +1,108 @@
-//
+const db = require('../../../database/database.js')
+
+// agregar cuenta
+async function addAccount(account) {
+    //account = {all}
+    try{
+        const [result] = await db.execute(`INSERT INTO users (name, lastName, img, email, pass, lvl, nReads, type, warning) VALUES (?,?,?,?,?,?,?,?,?)`, [account.name, account.lastName, account.img, account.email, account.pass, account.lvl, account.nReads, account.type, account.warning]);
+        return result;
+    } catch (error){
+        console.error('Error en addAccount');
+        throw error;
+    }
+}
+
+
+// eliminar cuenta
+async function deleteAccount(field) {
+    try{
+        const [result] = await db.execute(`DELETE FROM users WHERE id = ?`, [field]);
+        return result;
+    } catch (error){
+        console.error('Error en deleteAccount');
+        throw error;
+    }
+}
+
+
+// agregar medalla a un usuario
+async function addMedal(field) {
+    try{
+        const [result] = await db.execute(``);
+        return result;
+    } catch (error){
+        console.error('Error en ');
+        throw error;
+    }
+}
+
+
+// actualizar nivel
+async function updateLvl(field) {
+    try{
+        const [result] = await db.execute(``);
+        return result;
+    } catch (error){
+        console.error('Error en updateRank');
+        throw error;
+    }
+}
+
+
+// obtener lista de usuarios
+async function getUsers() {
+    try{
+        const [result] = await db.execute(`SELECT * FROM users`);
+        return result;
+    } catch (error){
+        console.error('Error en getUsers');
+        throw error;
+    }
+}
+
+
+// obtener lista de usuarios
+async function getUser(idUser) {
+    try{
+        const [result] = await db.execute(`SELECT * FROM users WHERE id = ?`, [idUser]);
+        return result;
+    } catch (error){
+        console.error('Error en getUser');
+        throw error;
+    }
+}
+
+
+// banear cuenta
+/*async function banUser(field) {
+    try{
+        const [result] = await db.execute(``);
+        return result;
+    } catch (error){
+        console.error('Error en ');
+        throw error;
+    }
+}
+*/
+
+// advertir usuario (cambiar su warning)
+async function warnUser(idUser, field) {
+    try{
+        const [result] = await db.execute(`UPDATE users SET warning = ? WHERE id = ?`, [field, idUser]);
+        return result;
+    } catch (error){
+        console.error('Error en warnUser');
+        throw error;
+    }
+}
+
+
+module.exports = {
+    addAccount,
+    deleteAccount,
+    addMedal,
+    updateLvl,
+    getUsers,
+    getUser,
+    warnUser
+}
