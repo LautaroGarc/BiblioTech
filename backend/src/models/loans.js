42c8ecda135301d261811d4e7b0988c3206a7854
garzodemierd <lautaro.garcia.et21.21@gmail.com>
2025-10-21 09:27:26 -0300

    loans & tems sql handlers

--- backend/src/models/loans.js
+++ backend/src/models/loans.js
@@ -1,11 +1,33 @@
 const db = require('../../../database/database.js')
 
 //obtener todos los prestamos
+async function getLoans(type = null) {
+
+};
 
 //obtener x prestamo (item - libro)
+async function getLoan(id, type) {
+
+};
 
 //obtener prestamo (item - libro) con x dato
+async function getLoan(id, type, field, data) {
+
+};
 
 //editar prestamos
+async function editLoan(id, type, field, data) {
+
+};
 
 //crear prestamos
+async function createLoan() {
+
+};
+
+module.exports = {
+    getLoans,
+    getLoan,
+    editLoan,
+    createLoan
+}
