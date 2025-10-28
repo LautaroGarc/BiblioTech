9da66176cdeb36f58c997a07fea86e8454c9360d
garzodemierd <lautaro.garcia.et21.21@gmail.com>
2025-10-28 19:52:33 -0300

    Login / Register API funcional

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv')
+
 const app = express();
-
-
-
-const PORT = '';
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log('Servidor disponible...')
+    console.log('[ Servidor disponible ]')
 })
 
 ///// cerrar servidor graceful /////////////////////////////////////////////
 
 process.on('SIGTERM', () => {
-    console.log('SIGTERM recibido, cerrando servidor...');
+    console.log('[ SIGTERM recibido, cerrando servidor ]');
     process.exit(0);
 });
 
 process.on('SIGINT', () => {
-    console.log('SIGINT recibido, cerrando servidor...');
+    console.log('[ SIGINT recibido, cerrando servidor ]');
     process.exit(0);
 });
 
