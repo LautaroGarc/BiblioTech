f823564f632e253e2309121e769ec3be615732a8
garzodemierd <lautaro.garcia.et21.21@gmail.com>
2025-10-28 21:42:01 -0300

    FIX FILES 28/10

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,3 +1,11 @@
-//- verifyToken() - Verificar JWT
-//- isAdmin() - Verificar si es docente/admin
-//- isStudent() - Verificar si es alumno
+// Verifica que email y password no estén vacíos
+
+// Verifica que:
+// - El email sea válido
+// - La contraseña tenga mínimo 6 caracteres
+// - Los campos no estén vacíos
+
+// Verifica que el token JWT sea válido
+// Si es válido: permite continuar
+// Si no es válido: devuelve error 401 (No autorizado)
+// devuelve el tipo de usuario
